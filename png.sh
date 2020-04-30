8d70eff699d206bda2c37d53048c527e13fbeebb
Shaw Summa <shawsumma@gmail.com>
2020-04-30 13:12:37 -0700

    remove files

--- png.sh
+++
@@ -1,5 +0,0 @@
-: rm trace.def trace.log out.dot
-dub build --compiler=ldc2 --build=profile
-    ./d9c $1 && \
-    dub run -q profdump -- -d > out.dot && \
-    dot -Tpng out.dot -o out.png
--- time.sh
+++
@@ -1,2 +0,0 @@
-dub build --quiet --compiler=ldc2 --build=release && \
-    time ./d9c $1 
