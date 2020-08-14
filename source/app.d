c11852bdcab0240682ee8521013c7f16118d23a3
Shaw Summa <ShawSumma@gmail.com>
2020-08-13 20:57:39 -0400

    actually uncomment main

--- source/app.d
+++ source/app.d
@@ -39,73 +39,42 @@ enum string getfile(string file)()
     return retval.to!string;
 }
 
-// void main(string[] args)
-// {
-//     // string[] scripts;
-//     // string[] stmts;
-//     // bool repl = false;
-//     // auto info = getopt(args, "repl", &repl, "eval", &stmts, "file", &scripts);
-//     // if (info.helpWanted)
-//     // {
-//     //     defaultGetoptPrinter("Help for 9c language.", info.options);
-//     //     return;
-//     // }
-//     // foreach (i; stmts)
-//     // {
-//     //     Node node = i.parse;
-//     //     Walker walker = new Walker;
-//     //     Function func = walker.walkProgram(node);
-//     //     func.captured = loadBase;
-//     //     Dynamic retval = run(func);
-//     //     if (retval.type != Dynamic.Type.nil)
-//     //     {
-//     //         writeln(retval);
-//     //     }
-//     // }
-//     // foreach (i; scripts ~ args[1 .. $])
-//     // {
-//     //     string code = cast(string) i.read;
-//     //     Node node = code.parse;
-//     //     // Typer typer = new Typer;
-//     //     // typer.annot(node);
-//     //     Walker walker = new Walker;
-//     //     Function func = walker.walkProgram(node);
-//     //     func.captured = loadBase;
-//     //     run(func);
-//     // }
-//     // if (repl || (args.length == 1 && scripts.length == 0 && stmts.length == 0))
-//     // {
-//     //     replRun;
-//     //     // while (true)
-//     //     // {
-//     //     // 	try
-//     //     // 	{
-//     //     // 		write(">>> ");
-//     //     // 		string code = readln.strip;
-//     //     // 		if (code.length == 0)
-//     //     // 		{
-//     //     // 			continue;
-//     //     // 		}
-//     //     // 		code = code;
-//     //     // 		Node node = code.parse;
-//     //     // 		Walker walker = new Walker;
-//     //     // 		Function func = walker.walkProgram(node);
-//     //     // 		func.captured = loadBase;
-//     //     // 		Dynamic retval = void;
-//     //     // 		retval = run(func);
-//     //     // 		if (retval.type != Dynamic.Type.nil)
-//     //     // 		{
-//     //     // 			writeln(retval);
-//     //     // 		}
-//     //     // 		foreach (i, ref v; glocals)
-//     //     // 		{
-//     //     // 			rootBase ~= Pair(func.stab.byPlace[i], v);
-//     //     // 		}
-//     //     // 	}
-//     //     // 	catch (Exception e)
-//     //     // 	{
-//     //     // 		writeln(e.msg);
-//     //     // 	}
-//     //     // }
-//     // }
-// }
+void main(string[] args)
+{
+    string[] scripts;
+    string[] stmts;
+    bool repl = false;
+    auto info = getopt(args, "repl", &repl, "eval", &stmts, "file", &scripts);
+    if (info.helpWanted)
+    {
+        defaultGetoptPrinter("Help for 9c language.", info.options);
+        return;
+    }
+    foreach (i; stmts)
+    {
+        Node node = i.parse;
+        Walker walker = new Walker;
+        Function func = walker.walkProgram(node);
+        func.captured = loadBase;
+        Dynamic retval = run(func);
+        if (retval.type != Dynamic.Type.nil)
+        {
+            writeln(retval);
+        }
+    }
+    foreach (i; scripts ~ args[1 .. $])
+    {
+        string code = cast(string) i.read;
+        Node node = code.parse;
+        // Typer typer = new Typer;
+        // typer.annot(node);
+        Walker walker = new Walker;
+        Function func = walker.walkProgram(node);
+        func.captured = loadBase;
+        run(func);
+    }
+    if (repl || (args.length == 1 && scripts.length == 0 && stmts.length == 0))
+    {
+        replRun;
+    }
+}
