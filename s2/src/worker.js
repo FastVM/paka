eef24d5584c342fe9c8688ce85c9f1ebda8ef3cb
Shaw Summa <shawsumma@gmail.com>
2021-06-20 23:49:53 -0400

    ready for compile hosting

---
+++ s2/src/worker.js
@@ -0,0 +1,57 @@
+const runFile = function(stream, putchar) {
+    var stdout='';
+
+    let mem;
+    const fd_write = function(fd, iovs, iovsLen, nwritten) {
+        var view = new DataView(mem.buffer);
+        var written = 0;
+        function getiovs(iovs, iovsLen) {
+            var buffers = Array.from({ length: iovsLen }, function (_, i) {
+                var ptr = iovs + i * 8;
+                var buf = view.getUint32(ptr, !0);
+                var bufLen = view.getUint32(ptr + 4, !0);
+
+                return new Uint8Array(mem.buffer, buf, bufLen);
+            });
+
+            return buffers;
+        }
+        var buffers = getiovs(iovs, iovsLen);
+        function writev(iov) {
+            for (var b = 0; b < iov.byteLength; b++) {
+                putchar(iov[b]);
+            }
+            written += b;
+        }
+        buffers.forEach(writev);
+        view.setUint32(nwritten, written, !0);
+        return 0;
+    };
+
+    return WebAssembly.instantiateStreaming(stream, {wasi_unstable: {fd_write}}).then(res => {
+        mem = res.instance.exports.memory;
+        putchar('__BEGIN__');
+        res.instance.exports.main();
+        putchar('__END__');
+    });
+}
+
+function compile(src, putchar) {
+    const stream = fetch('/api/wasm', {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'text/plain',
+        },
+        redirect: 'follow',
+        body: src,
+    })
+    runFile(stream, putchar);
+}
+
+onmessage = function(e) {
+    compile(e.data, function(c) {
+        postMessage(c);
+    });
+};
---
+++ s2/webpack.config.js
@@ -0,0 +1,50 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = "style-loader";
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MonacoWebpackPlugin({}),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+
+  } else {
+    config.mode = "development";
+  }
+  config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  return config;
+};
